f39919d9f969383c4a34d1c7e952e64bd6c6c0fd
Mahipal Gurjala <Mahipal.Gurjala@planonsoftware.com>
2019-05-03 11:54:38 +0530

    Updates

---
+++ precache-manifest.6717472ba26298d9bbcd176c5d9469fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f90fa286da2f834095a8",
+    "url": "/React-Test-Async/static/js/runtime~main.8eec8833.js"
+  },
+  {
+    "revision": "ec8a3af5429773cad033",
+    "url": "/React-Test-Async/static/js/main.87492f6b.chunk.js"
+  },
+  {
+    "revision": "e85f5e4261933feb18a6",
+    "url": "/React-Test-Async/static/js/2.fb9331f4.chunk.js"
+  },
+  {
+    "revision": "ec8a3af5429773cad033",
+    "url": "/React-Test-Async/static/css/main.2b5bd772.chunk.css"
+  },
+  {
+    "revision": "0aac665d56805fc03300120c68d47a13",
+    "url": "/React-Test-Async/index.html"
+  }
+];
